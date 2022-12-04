7b1b514d69a2ad683cb68359fc86b4e7b708d1c0
Sidorik Ilia <iss19.spb@gmail.com>
2022-12-04 23:52:48 +0700

    Add UITabBarController, 2x UINavigationController, 2x root ViewController, 2x items

---
+++ Navigation/TabBarController/FeedVC/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LineTabVC.swift
+//  Navigation
+//
+//  Created by Илья Сидорик on 04.12.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        //code
+    }
+}
---
+++ Navigation/TabBarController/ProfileVC/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoTabCV.swift
+//  Navigation
+//
+//  Created by Илья Сидорик on 04.12.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        //code
+    }
+}
